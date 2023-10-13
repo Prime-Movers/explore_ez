f760715d0ba4681ed31667548dd65c1ec2ef3cd5
barathan <barathank76@gmail.com>
2023-10-13 23:09:12 +0530

    added bloc state management

--- lib/UI/login_page.dart
+++ lib/UI/login_page.dart
@@ -1,10 +1,283 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../Bloc/loginbloc.dart';
+import 'package:explore_ez/bloc/my_form_bloc.dart';
+import 'package:formz/formz.dart';
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: BlocProvider(
+          create: (_) => MyFormBloc(),
+          child: Stack(children: [
+          Image.asset(
+            'assets/images/login_screen_background_image.jpg',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.6),
+
+          ),
+          const Center(child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Welcome to Explore EZ',
+                    style: TextStyle(
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  MyForm(),
+          ],)
+        ),
+      ),],
+    ),),),);
+  }
+}
+
+class MyForm extends StatefulWidget {
+  const MyForm({super.key});
+
+  @override
+  State<MyForm> createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MyForm> {
+  final _emailFocusNode = FocusNode();
+  final _passwordFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _emailFocusNode.addListener(() {
+      if (!_emailFocusNode.hasFocus) {
+        context.read<MyFormBloc>().add(EmailUnfocused());
+        FocusScope.of(context).requestFocus(_passwordFocusNode);
+      }
+    });
+    _passwordFocusNode.addListener(() {
+      if (!_passwordFocusNode.hasFocus) {
+        context.read<MyFormBloc>().add(PasswordUnfocused());
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<MyFormBloc, MyFormState>(
+      listener: (context, state) {
+        if (state.status.isSuccess) {
+          ScaffoldMessenger.of(context).hideCurrentSnackBar();
+          showDialog<void>(
+            context: context,
+            builder: (_) => const SuccessDialog(),
+          );
+        }
+        if (state.status.isInProgress) {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              const SnackBar(content: Text('Submitting...')),
+            );
+        }
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: <Widget>[
+            EmailInput(focusNode: _emailFocusNode),
+            const SizedBox(height:20.0),
+            PasswordInput(focusNode: _passwordFocusNode),
+            const SizedBox(height:20.0),
+             Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+            const loginButton(),ElevatedButton(
+                        onPressed: () {
+                          // Implement registration logic
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 202, 121, 234),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 30.0, vertical: 15.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        child: const Text(
+                          'Register',
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                      ),],),
+          ],
+        ),
+      ),
+    );
+  }
+}
+class background extends StatelessWidget {
+  const background({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+class EmailInput extends StatelessWidget {
+  const EmailInput({required this.focusNode, super.key});
+
+  final FocusNode focusNode;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MyFormBloc, MyFormState>(
+      builder: (context, state) {
+        return TextFormField(
+          initialValue: state.email.value,
+          focusNode: focusNode,
+          decoration: InputDecoration(
+            hintText: "Enter your email",
+            fillColor: Colors.white,
+            filled: true,
+            border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+            errorText: state.email.displayError != null
+                ? 'Please ensure the email entered is valid'
+                : null,
+          ),
+          keyboardType: TextInputType.emailAddress,
+          onChanged: (value) {
+            context.read<MyFormBloc>().add(EmailChanged(email: value));
+          },
+          textInputAction: TextInputAction.next,
+          style: const TextStyle(color: Colors.black),
+        );
+      },
+    );
+  }
+}
+
+class PasswordInput extends StatelessWidget {
+  const PasswordInput({required this.focusNode, super.key});
+
+  final FocusNode focusNode;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MyFormBloc, MyFormState>(
+      builder: (context, state) {
+        return TextFormField(
+          initialValue: state.password.value,
+          focusNode: focusNode,
+          decoration: InputDecoration(
+            hintText: "Enter your password",
+            fillColor: Colors.white,
+            filled: true,
+             border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+            helperMaxLines: 2,
+            
+            errorMaxLines: 2,
+            errorText: state.password.displayError != null
+                ? '''Password must be at least 8 characters and contain at least one letter and number'''
+                : null,
+          ),
+          style: const TextStyle(color: Colors.black),
+          obscureText: true,
+          onChanged: (value) {
+            context.read<MyFormBloc>().add(PasswordChanged(password: value));
+          },
+          textInputAction: TextInputAction.done,
+        );
+      },
+    );
+  }
+}
+
+class loginButton extends StatelessWidget {
+  const loginButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final isValid = context.select((MyFormBloc bloc) => bloc.state.isValid);
+    return ElevatedButton(
+      onPressed: isValid
+          ? () => context.read<MyFormBloc>().add(FormSubmitted())
+          : null,
+      child: const Text('login'),
+      
+      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 40.0, vertical: 16.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),),
+    );
+  }
+}
+
+class SuccessDialog extends StatelessWidget {
+  const SuccessDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const Row(
+              children: <Widget>[
+                Icon(Icons.info),
+                Flexible(
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'logined Successfully!',
+                      softWrap: true,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ElevatedButton(
+              child: const Text('OK'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -133,16 +406,7 @@ class _LoginPageState extends State<LoginPage> {
                         ),
                       ),
                     ],
-                  ), /*
-                      } else if (state is LoginSuccessState) {
-                        return const Text('Login Success!');
-                      } else if (state is LoginErrorState) {
-                        return Text('Error: ${state.error}');
-                      } else {
-                        return const CircularProgressIndicator();
-                      }
-                    },
-                  ),*/
+                  ), 
                 ],
               ),
             ),
@@ -152,3 +416,4 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
+*/
