f8ca4bd2bb998cdbb4c23a98bb7f4966e5129f11
Bangarrumurali <bangarrumurali@gmail.com>
2024-02-13 20:27:30 +0530

    water
    home screen,details,vertical added

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../Details.dart';
+import '../../components/vertical_place_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,8 +9,40 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.onBackground,
-      body: const Center(
-        child: Text("Hello there"),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              "Where are you \ngoing?",
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: SearchBar(),
+            buildVerticalList(),
+          )
+        ],
+      ),
+    );
+  }
+
+  buildVerticalList() {
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: ListView.builder(
+        primary: false,
+        physics: NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        itemCount: places == null ? 0 : places.length,
+        itemBuilder: (BuildContext context, int index) {
+          Map place = places[index];
+          return VerticalPlaceItem(place: place);
+        },
       ),
     );
   }
