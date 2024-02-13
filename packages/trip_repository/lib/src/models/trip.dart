0709702a0879d5353bc020ae2a28692df108850b
barathan <barathankarthikeyan06@gmail.com>
2024-02-13 14:14:26 +0530

    updated contents

--- packages/trip_repository/lib/src/models/trip.dart
+++ packages/trip_repository/lib/src/models/trip.dart
@@ -1,36 +1,37 @@
+import 'package:area_repository/area_repository.dart';
+
 import '../entities/entities.dart';
 
 class Trip {
   String area;
   String budget;
   int days;
-  List<dynamic> places;
-  List<dynamic> placesImage;
+  List<Place> place;
   String details;
-  Trip(
-      {required this.area,
-      required this.budget,
-      required this.days,
-      required this.places,
-      required this.details,
-      required this.placesImage});
+  Trip({
+    required this.area,
+    required this.budget,
+    required this.days,
+    required this.place,
+    required this.details,
+  });
   TripEntity toEntity() {
     return TripEntity(
-        area: area,
-        budget: budget,
-        days: days,
-        places: places,
-        details: details,
-        placesImage: placesImage);
+      area: area,
+      budget: budget,
+      days: days,
+      place: place,
+      details: details,
+    );
   }
 
   static Trip fromEntity(TripEntity entity) {
     return Trip(
-        area: entity.area,
-        budget: entity.budget,
-        days: entity.days,
-        places: entity.places,
-        details: entity.details,
-        placesImage: entity.placesImage);
+      area: entity.area,
+      budget: entity.budget,
+      days: entity.days,
+      place: entity.place,
+      details: entity.details,
+    );
   }
 }
