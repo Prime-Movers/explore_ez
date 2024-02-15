123320316528c8f2339bcb051bf7e391efc965ca
Dhaya182 <dhayanand182@gmail.com>
2024-02-15 13:37:34 +0530

    feat:Gemini_UI

--- gemini_model/lib/main.dart
+++ gemini_model/lib/main.dart
@@ -1,5 +1,11 @@
+// ignore: unnecessary_import
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gemini_model/api/gemini_api.dart';
+import 'package:get/get.dart';
 
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:google_gemini/google_gemini.dart';
 void main() {
   runApp(const MainApp());
 }
@@ -10,11 +16,62 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        title: 'Flutter Demo',
+        // theme: ThemeData(
+        //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        //   useMaterial3: true,
+        //  ),
+        home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    RxString result = ''.obs;
+    return SafeArea(
+        child: Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                keyboardType: TextInputType.text,
+                controller: textController,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                  onPressed: () async {
+                    result.value =
+                        await GeminiAPI.getGeminiData(textController.text);
+                  },
+                  child: const Text(
+                    'generate',
+                    style: TextStyle(color: Colors.black),
+                  )),
+              const SizedBox(height: 20),
+              Obx(
+                () => Text(
+                  result.value,
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    );
+    ));
   }
 }
