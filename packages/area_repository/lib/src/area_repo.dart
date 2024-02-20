87eace974347862ed6a74755f542064c6ab9c1a4
barathan <barathankarthikeyan06@gmail.com>
2024-02-20 20:43:46 +0530

    removed unwanted code

--- packages/area_repository/lib/src/area_repo.dart
+++ packages/area_repository/lib/src/area_repo.dart
@@ -1,8 +1,6 @@
 import 'models/models.dart';
 
 abstract class AreaRepo {
-  Future<List<MyArea>> getAreas();
-  Future<List<MyArea>> searchArea(String value);
   Future<List<String>> getAreaNames();
   Future<List<Place>> getPlaces(String areaName);
   Future<List<String>> searchAreaNames(String value);
--- packages/area_repository/lib/src/firebase_area_repo.dart
+++ packages/area_repository/lib/src/firebase_area_repo.dart
@@ -5,17 +5,6 @@ import 'package:area_repository/area_repository.dart';
 
 class FirebaseAreaRepo implements AreaRepo {
   final areaCollection = FirebaseFirestore.instance.collection('Areas');
-  @override
-  Future<List<MyArea>> getAreas() async {
-    try {
-      return await areaCollection.get().then((value) => value.docs
-          .map((e) => MyArea.fromEntity(MyAreaEntity.fromDocument(e.data())))
-          .toList());
-    } catch (e) {
-      log(e.toString());
-      rethrow;
-    }
-  }
 
   @override
   Future<List<String>> getAreaNames() async {
@@ -39,30 +28,6 @@ class FirebaseAreaRepo implements AreaRepo {
                   (e) => MyArea.fromEntity(MyAreaEntity.fromDocument(e.data())))
               .first
               .places
-              .toList());
-    } catch (e) {
-      log(e.toString());
-      rethrow;
-    }
-  }
-
-  @override
-  Future<List<MyArea>> searchArea(String value) async {
-    if (value.isEmpty) {
-      // Return all areas if search query is empty
-      return await getAreas();
-    }
-    try {
-      return await areaCollection
-          .where(
-            'areaName',
-            isGreaterThanOrEqualTo: value,
-            isLessThanOrEqualTo: '${value}z', // Consider using endAt here
-          )
-          .get()
-          .then((value) => value.docs
-              .map(
-                  (e) => MyArea.fromEntity(MyAreaEntity.fromDocument(e.data())))
               .toList());
     } catch (e) {
       log(e.toString());
