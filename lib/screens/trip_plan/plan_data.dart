ead36f703049c2dd5cc338fc62b584e4d07eb5d8
barathan <barathankarthikeyan06@gmail.com>
2024-02-25 19:56:10 +0530

    added changes in the code

--- lib/screens/trip_plan/plan_data.dart
+++ lib/screens/trip_plan/plan_data.dart
@@ -1,17 +1,32 @@
 import 'package:explore_ez/blocs/plan_details_bloc/plan_details_bloc.dart';
+import 'package:explore_ez/blocs/tour_plan_model_bloc/tour_plan_model_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:plan_repository/plan_repository.dart';
 
 class PlanData extends StatelessWidget {
   const PlanData({super.key});
 
   @override
   Widget build(BuildContext context) {
-    MyPlan plan = context.read<PlanDetailsBloc>().state.plan;
+    BlocProvider.of<TourPlanModelBloc>(context)
+        .add(GetTourPlan(plan: context.read<PlanDetailsBloc>().state.plan));
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.onBackground,
-      body: Center(child: Text(plan.area)),
+      body: BlocBuilder<TourPlanModelBloc, TourPlanModelState>(
+        builder: (context, state) {
+          if (state is TourPlanModelLoading) {
+            return const CircularProgressIndicator();
+          } else if (state is TourPlanModelSuccess) {
+            return Center(
+              child: Text(state.tourPlan),
+            );
+          } else {
+            return const Center(
+              child: Text("An Error Occurred"),
+            );
+          }
+        },
+      ),
     );
   }
 }
