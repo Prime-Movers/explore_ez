ee7705444671bff3fe91095b1464b3ab38778e0d
Dhaya182 <dhayanand182@gmail.com>
2024-02-25 17:05:55 +0530

    fix:distance_matrix
    Updated code in distance matrix

--- python_flask_gemini/distance_matrix.py
+++ python_flask_gemini/distance_matrix.py
@@ -14,32 +14,37 @@ import itertools
 from python_tsp.exact import solve_tsp_dynamic_programming
 from python_tsp.heuristics import solve_tsp_simulated_annealing
 
-places=["Snow Kingdom","Kapaleeshwarar Temple","Besant Nagar Beach","Marina Beach","San Thome Church"]
 
-result =  []
-distance_dict={}
-for place1 in places:
-  for place2 in places:
-    direction_result=gmaps_client.directions(place1,place2,mode="driving")
-    a=place1+" "+place2
-    result.append((a,direction_result[0]['legs'][0]['distance']['text']))
-    distance_dict[place1,place2]=direction_result[0]['legs'][0]['distance']['value']
-distance=[]
-for place in places:
-  distance_1d=[]
-  for place2 in places:
-    distance_1d.append(distance_dict[place,place2]/1000)
-  distance.append(distance_1d)
 
-distance_matrix = np.array(distance)
+class TSPModel:
 
-permutation,distance = solve_tsp_simulated_annealing(distance_matrix)
-# permutation,distance = solve_tsp_dynamic_programming(distance_matrix)
-permutation
+  def __init__(self,places):
+    # places=["Snow Kingdom","Kapaleeshwarar Temple","Besant Nagar Beach","Marina Beach","San Thome Church"]
 
-route = []
-for i in permutation:
-  route.append(places[i])
+    result =  []
+    distance_dict={}
+    for place1 in places:
+      for place2 in places:
+        direction_result=gmaps_client.directions(place1,place2,mode="driving")
+        a=place1+" "+place2
+        result.append((a,direction_result[0]['legs'][0]['distance']['text']))
+        distance_dict[place1,place2]=direction_result[0]['legs'][0]['distance']['value']
+    distance=[]
+    for place in places:
+      distance_1d=[]
+      for place2 in places:
+        distance_1d.append(distance_dict[place,place2]/1000)
+      distance.append(distance_1d)
 
-print(route)
+    distance_matrix = np.array(distance)
+
+    permutation,distance = solve_tsp_simulated_annealing(distance_matrix)
+    # permutation,distance = solve_tsp_dynamic_programming(distance_matrix)
+    permutation
+
+    route = ""
+    for i in permutation:
+      route+=i
+      route+=", "
+    return route
 
