9f91447400f56b5dec4eb0ad8e5e1a5b825dc0bf
Dhaya182 <dhayanand182@gmail.com>
2024-02-26 14:37:09 +0530

    fix:functio request

--- packages/plan_repository/lib/src/function.dart
+++ packages/plan_repository/lib/src/function.dart
@@ -4,8 +4,5 @@ import 'package:http/http.dart' as http;
 
 fetchdata(String url) async {
   http.Response response = await http.get(Uri.parse(url));
-  if (response.body == null) {
-    return {};
-  }
   return response.body;
 }
