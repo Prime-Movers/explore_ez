753ca2a4b7303df9fa013d9618e68e3f00f71e7f
barathan <barathankarthikeyan06@gmail.com>
2024-03-03 09:36:21 +0530

    removed unwanted code in textfield widget

--- lib/components/textfield.dart
+++ lib/components/textfield.dart
@@ -4,58 +4,104 @@ class MyTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
   final bool obscureText;
-	final TextInputType keyboardType;
-	final Widget? suffixIcon;
-	final VoidCallback? onTap;
-	final Widget? prefixIcon;
-	final String? Function(String?)? validator;
-	final FocusNode? focusNode;
-	final String? errorMsg;
-	final String? Function(String?)? onChanged;
+  final TextInputType keyboardType;
+  final Widget? suffixIcon;
+  final VoidCallback? onTap;
+  final Widget? prefixIcon;
+  final String? Function(String?)? validator;
+  final FocusNode? focusNode;
+  final String? errorMsg;
+  final String? Function(String?)? onChanged;
 
-  const MyTextField({
-    super.key,
-    required this.controller,
-    required this.hintText,
-    required this.obscureText,
-		required this.keyboardType,
-		this.suffixIcon,
-		this.onTap,
-		this.prefixIcon,
-		this.validator,
-		this.focusNode,
-		this.errorMsg,
-		this.onChanged
-  });
+  const MyTextField(
+      {super.key,
+      required this.controller,
+      required this.hintText,
+      required this.obscureText,
+      required this.keyboardType,
+      this.suffixIcon,
+      this.onTap,
+      this.prefixIcon,
+      this.validator,
+      this.focusNode,
+      this.errorMsg,
+      this.onChanged});
 
-	@override
+  @override
   Widget build(BuildContext context) {
     return TextFormField(
       validator: validator,
       controller: controller,
       obscureText: obscureText,
-			keyboardType: keyboardType,
-			focusNode: focusNode,
-			onTap: onTap,
-			textInputAction: TextInputAction.next,
-			onChanged: onChanged,
+      keyboardType: keyboardType,
+      focusNode: focusNode,
+      onTap: onTap,
+      textInputAction: TextInputAction.next,
+      onChanged: onChanged,
       decoration: InputDecoration(
-				suffixIcon: suffixIcon,
-				prefixIcon: prefixIcon,
-				enabledBorder: OutlineInputBorder(
-					borderRadius: BorderRadius.circular(10),
-					borderSide: const BorderSide(color: Colors.transparent),
-				),
-				focusedBorder: OutlineInputBorder(
-					borderRadius: BorderRadius.circular(20),
-					borderSide: BorderSide(color: Theme.of(context).colorScheme.secondary),
-				),
-				fillColor: Colors.grey.shade200,
-				filled: true,
-				hintText: hintText,
-				hintStyle: TextStyle(color: Colors.grey[500]),
-				errorText: errorMsg,
-			),
+        suffixIcon: suffixIcon,
+        prefixIcon: prefixIcon,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(color: Colors.transparent),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide:
+              BorderSide(color: Theme.of(context).colorScheme.secondary),
+        ),
+        fillColor: Colors.grey.shade200,
+        filled: true,
+        hintText: hintText,
+        hintStyle: TextStyle(color: Colors.grey[500]),
+        errorText: errorMsg,
+      ),
+    );
+  }
+}
+
+class TextDetailsWidget extends StatelessWidget {
+  final String labelText;
+  final IconData prefixIcon;
+  final VoidCallback? onTap;
+  final TextEditingController controller;
+  final String validation;
+  const TextDetailsWidget(
+      {super.key,
+      required this.labelText,
+      required this.prefixIcon,
+      required this.onTap,
+      required this.controller,
+      required this.validation});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      decoration: InputDecoration(
+        labelText: labelText,
+        prefixIcon: Icon(
+          prefixIcon,
+          color: Theme.of(context).colorScheme.primary,
+        ),
+        hintStyle: TextStyle(color: Theme.of(context).colorScheme.background),
+        contentPadding: const EdgeInsets.all(12.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ),
+      ),
+      readOnly: true,
+      onTap: onTap,
+      controller: controller,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return validation;
+        }
+        return null;
+      },
     );
   }
 }
