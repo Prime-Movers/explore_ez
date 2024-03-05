e1be6b3883860d943bcc9a9cfac75b663819414c
Bangarrumurali <bangarrumurali@gmail.com>
2024-03-05 08:47:29 +0530

    added links

--- packages/plan_repository/lib/src/model_plan_repo.dart
+++ packages/plan_repository/lib/src/model_plan_repo.dart
@@ -25,8 +25,8 @@ class ModelPlanRepo implements PlanRepo {
       value += plan.places[plan.places.length - 1].placeName + " " + "chennai";
       //url = "https://musical-easily-yak.ngrok-free.app/?query=" + value;
       // url = 'http://10.0.2.2:5000/?query=' + value;
-      url =
-          "https://presumably-welcomed-giraffe.ngrok-free.app/?query=" + value;
+      //url ="https://presumably-welcomed-giraffe.ngrok-free.app/?query=" + value;
+      url = "https://endlessly-wise-chigger.ngrok-free.app/?query=" + value;
 
       final String ans = await getdata(url);
 
