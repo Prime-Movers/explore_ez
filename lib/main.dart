6332a25cafc2aae92be9b1c0f1d0ee3423f5d7e1
barathan76 <barathankarthikeyan06@gmail.com>
2025-10-04 23:22:14 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,15 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_repository/user_repository.dart';
 
-import 'firebase_options.dart';
+// import 'firebase_options.dart';
 import 'app.dart';
 import 'simple_bloc_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+      // options: DefaultFirebaseOptions.currentPlatform,
+      );
   Bloc.observer = SimpleBlocObserver();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(MainApp(FirebaseUserRepo()));
